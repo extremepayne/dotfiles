135d0a7bb39d553e0058e0d648cc9ba7bc2141d1
payne <harrison.payne@gmail.com>
2022-10-03 14:31:40 -0600

    Start picom with experimental backends

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
-picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
+picom --experimental-backends & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
 # Low battery notifier
 ~/.config/qtile/scripts/check_battery.sh & disown
