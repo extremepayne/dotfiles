90e5453b83703ce5b01edfd481ae3c95dc3c9c9a
payne <harrison.payne@gmail.com>
2024-01-01 00:16:48 -0700

    (qtile) start unclutter and autorandr on startup

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 picom -b & # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
+unclutter &
+
+autorandr --change &
+
 # Low battery notifier
 ~/.config/qtile/scripts/check_battery.sh & disown
 
