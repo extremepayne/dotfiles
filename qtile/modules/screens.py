05a68f446d1e3443e4580d7505ee5fc4af4bb35a
payne <harrison.payne@gmail.com>
2024-01-06 16:55:29 -0700

    (qtile) attempt to make maximizing work with multiple screens

--- qtile/modules/screens.py
+++ qtile/modules/screens.py
@@ -3,6 +3,7 @@ from .widgets import *
 from .colors import colors
 from libqtile.config import Screen
 from libqtile import hook, log_utils
+from libqtile.log_utils import logger
 
 # from modules.keys import terminal
 import os
@@ -156,19 +157,79 @@ screens = [
     ),
 ]
 
+safe_to_modify_bar = False
 
+@hook.subscribe.startup_complete
+def its_safe_now():
+    global safe_to_modify_bar
+    safe_to_modify_bar = True
+
+# @hook.subscribe.layout_change
+# def max_no_margins(layout, group):
+    # if safe_to_modify_bar:
+        # if qtile.current_screen == screens[0]:
+            # my_change_bar = main_bar
+        # elif qtile.current_screen == screens[1]:
+            # my_change_bar = secondary_bar
+        # else:
+            # logger.warning("You have more than two screens and your config doesn't account for that")
+            # my_change_bar = main_bar
+        # if qtile.current_layout.name == "max":
+            # my_change_bar.margin = [0, 0, 0, 0]
+            # my_change_bar._initial_margin = [0, 0, 0, 0]
+            # my_change_bar._configure(qtile, qtile.current_screen, reconfigure=True)
+            # my_change_bar.draw()
+            # qtile.current_screen.group.layout_all()
+    # 
+        # else:
+            # pass
+            # my_change_bar.margin = [8, 8, 0, 8]
+            # my_change_bar._initial_margin = [8, 8, 0, 8]
+            # my_change_bar._configure(qtile, qtile.current_screen, reconfigure=True)
+            # my_change_bar.draw()
+            # qtile.current_screen.group.layout_all()
 @hook.subscribe.layout_change
 def max_no_margins(layout, group):
-    if qtile.current_layout.name == "max":
-        my_bar.margin = [0, 0, 0, 0]
-        my_bar._initial_margin = [0, 0, 0, 0]
-        my_bar._configure(qtile, qtile.current_screen, reconfigure=True)
-        my_bar.draw()
-        qtile.current_screen.group.layout_all()
-
-    else:
-        my_bar.margin = [8, 8, 0, 8]
-        my_bar._initial_margin = [8, 8, 0, 8]
-        my_bar._configure(qtile, qtile.current_screen, reconfigure=True)
-        my_bar.draw()
-        qtile.current_screen.group.layout_all()
+    if safe_to_modify_bar:
+        logger.warning("~~~~~~~~max_no_margins CALLED~~~~~~~~~~~~~")
+        if qtile.current_layout.name == "max":
+            if qtile.current_screen == screens[0]:
+                main_bar.margin = [8, 8, 0, 8]
+                main_bar._initial_margin = [8, 8, 0, 8]
+                # main_bar._configure(qtile, qtile.current_screen, reconfigure=True)
+                main_bar.draw()
+                qtile.current_screen.group.layout_all()
+            elif qtile.current_screen == screens[1]:
+                secondary_bar.margin = [8, 8, 0, 8]
+                secondary_bar._initial_margin = [8, 8, 0, 8]
+                # secondary_bar._configure(qtile, qtile.current_screen, reconfigure=True)
+                secondary_bar.draw()
+                qtile.current_screen.group.layout_all()
+            else:
+                logger.warning("You have more than two screens and your config doesn't account for that")
+                main_bar.margin = [8, 8, 0, 8]
+                main_bar._initial_margin = [8, 8, 0, 8]
+                # main_bar._configure(qtile, qtile.current_screen, reconfigure=True)
+                main_bar.draw()
+                qtile.current_screen.group.layout_all()
+    
+        else:
+            if qtile.current_screen == screens[0]:
+                main_bar.margin = [8, 8, 0, 8]
+                main_bar._initial_margin = [8, 8, 0, 8]
+                # main_bar._configure(qtile, qtile.current_screen, reconfigure=True)
+                main_bar.draw()
+                qtile.current_screen.group.layout_all()
+            elif qtile.current_screen == screens[1]:
+                secondary_bar.margin = [8, 8, 0, 8]
+                secondary_bar._initial_margin = [8, 8, 0, 8]
+                # secondary_bar._configure(qtile, qtile.current_screen, reconfigure=True)
+                secondary_bar.draw()
+                qtile.current_screen.group.layout_all()
+            else:
+                logger.warning("You have more than two screens and your config doesn't account for that")
+                main_bar.margin = [8, 8, 0, 8]
+                main_bar._initial_margin = [8, 8, 0, 8]
+                # main_bar._configure(qtile, qtile.current_screen, reconfigure=True)
+                main_bar.draw()
+                qtile.current_screen.group.layout_all()
